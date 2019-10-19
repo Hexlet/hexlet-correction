da4decbbce0ec651c22f613d784f588e8be9952d
Timur Malikin <timur.malikin@gmail.com>
2019-10-19 21:36:55 +0300

    Add logout button (#47)

--- src/main/java/io/hexlet/hexletcorrection/config/WebSecurityConfig.java
+++ src/main/java/io/hexlet/hexletcorrection/config/WebSecurityConfig.java
@@ -36,6 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutSuccessUrl("/login")
                 .permitAll();
     }
 
