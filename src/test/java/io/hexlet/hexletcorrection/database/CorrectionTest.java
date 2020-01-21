1d015e08566344fdeba069259c88a79d1f924da1
Anatoly Samokisha <anatoly@samokisha.com>
2020-01-21 22:29:09 +0200

    Fixed fixtures test (#55)

--- src/test/java/io/hexlet/hexletcorrection/database/CorrectionTest.java
+++ src/test/java/io/hexlet/hexletcorrection/database/CorrectionTest.java
@@ -9,12 +9,12 @@ import io.hexlet.hexletcorrection.repository.CorrectionRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 @DBRider
 public class CorrectionTest {
 
