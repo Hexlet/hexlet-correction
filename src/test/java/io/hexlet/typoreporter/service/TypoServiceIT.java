644ddde0cfd4885d08c9733cbadc8d305d77b015
adm134 <adm134@yandex.ru>
2022-02-22 13:08:56 +0300

    TypoService tests

--- src/test/java/io/hexlet/typoreporter/service/TypoServiceIT.java
+++ src/test/java/io/hexlet/typoreporter/service/TypoServiceIT.java
@@ -3,28 +3,40 @@ package io.hexlet.typoreporter.service;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
+import io.hexlet.typoreporter.domain.AbstractAuditingEntity;
 import io.hexlet.typoreporter.domain.typo.Typo;
+import io.hexlet.typoreporter.domain.typo.TypoStatus;
 import io.hexlet.typoreporter.domain.workspace.Workspace;
 import io.hexlet.typoreporter.repository.TypoRepository;
-import io.hexlet.typoreporter.service.dto.typo.*;
+import io.hexlet.typoreporter.service.dto.typo.TypoInfo;
+import io.hexlet.typoreporter.service.dto.typo.TypoReport;
 import io.hexlet.typoreporter.test.DBUnitEnumPostgres;
 import io.hexlet.typoreporter.test.asserts.ReportedTypoAssert;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.*;
-import org.springframework.test.context.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.*;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 import static com.github.database.rider.core.api.configuration.Orthography.LOWERCASE;
 import static io.hexlet.typoreporter.domain.typo.TypoEvent.*;
 import static io.hexlet.typoreporter.domain.typo.TypoStatus.*;
 import static io.hexlet.typoreporter.test.Constraints.POSTGRES_IMAGE;
 import static io.hexlet.typoreporter.test.factory.EntitiesFactory.WORKSPACE_101_NAME;
+import static io.hexlet.typoreporter.test.factory.EntitiesFactory.WORKSPACE_103_NAME;
 import static io.hexlet.typoreporter.web.Routers.DEFAULT_SORT_FIELD;
 import static java.util.function.Predicate.not;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -154,5 +166,65 @@ class TypoServiceIT {
         assertThat(repository.existsById(id)).isFalse();
         assertThat(service.deleteTypoById(id)).isZero();
         assertThat(repository.existsById(id)).isFalse();
+    }
+
+    @Test
+    void getCountTypoByStatusForWorkspaceName() {
+
+        List<Typo> listAll = repository.findAll();
+
+        long uniqTypoStatus = listAll.stream()
+            .filter(x -> x.getWorkspace().getName().equals(WORKSPACE_101_NAME))
+            .map(Typo::getTypoStatus)
+            .distinct()
+            .count();
+
+        Long countReported = listAll.stream()
+            .filter(x -> x.getWorkspace().getName().equals(WORKSPACE_101_NAME))
+            .filter(x -> x.getTypoStatus().equals(REPORTED)).count();
+
+        Long countInProgress = listAll.stream()
+            .filter(x -> x.getWorkspace().getName().equals(WORKSPACE_101_NAME))
+            .filter(x -> x.getTypoStatus().equals(IN_PROGRESS)).count();
+
+        Long countResolved = listAll.stream()
+            .filter(x -> x.getWorkspace().getName().equals(WORKSPACE_101_NAME))
+            .filter(x -> x.getTypoStatus().equals(RESOLVED)).count();
+
+        Long countCanceled = listAll.stream()
+            .filter(x -> x.getWorkspace().getName().equals(WORKSPACE_101_NAME))
+            .filter(x -> x.getTypoStatus().equals(CANCELED)).count();
+
+        List<Pair<TypoStatus, Long>> listWks = service.getCountTypoByStatusForWorkspaceName(WORKSPACE_101_NAME);
+        assertThat(listWks.size()).isEqualTo((int) uniqTypoStatus);
+        assertThat(listWks.stream())
+            .filteredOn(x -> x.getLeft().equals(REPORTED) && x.getRight().equals(countReported))
+            .hasSize(1);
+        assertThat(listWks.stream())
+            .filteredOn(x -> x.getLeft().equals(IN_PROGRESS) && x.getRight().equals(countInProgress))
+            .hasSize(1);
+        assertThat(listWks.stream())
+            .filteredOn(x -> x.getLeft().equals(RESOLVED) && x.getRight().equals(countResolved))
+            .hasSize(1);
+        assertThat(listWks.stream())
+            .filteredOn(x -> x.getLeft().equals(CANCELED) && x.getRight().equals(countCanceled))
+            .hasSize(1);
+
+    }
+
+    @Test
+    void getLastTypoByWorkspaceName() {
+        Optional<Typo> Top = repository.findAll()
+            .stream()
+            .filter(x -> x.getWorkspace().getName().equals(WORKSPACE_101_NAME))
+            .sorted(Comparator.comparing(AbstractAuditingEntity::getCreatedDate))
+            .limit(1L)
+            .findFirst();
+
+        Optional<TypoInfo> typoInfo = service.getLastTypoByWorkspaceName(WORKSPACE_101_NAME);
+        assertThat(typoInfo).isNotEmpty();
+        assertThat(typoInfo.get().id()).isEqualTo(Top.get().getId());
+        Optional<TypoInfo> typoInfo2 = service.getLastTypoByWorkspaceName(WORKSPACE_103_NAME);
+        assertThat(typoInfo2).isEmpty();
     }
 }
