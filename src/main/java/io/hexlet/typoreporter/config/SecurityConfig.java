b297b37d1d3a31b2ba0778ccb59197b1dfefe6e8
Artem Gorbounov <artem.gorbounov@gmail.com>
2022-06-09 00:20:49 +0200

    Remove deprecated class WebSecurityConfigurerAdapter from SecurityConfig

--- src/main/java/io/hexlet/typoreporter/config/SecurityConfig.java
+++ src/main/java/io/hexlet/typoreporter/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import io.hexlet.typoreporter.security.provider.WorkspaceTokenAuthenticationProv
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.*;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.zalando.problem.spring.web.advice.AdviceTrait;
@@ -18,11 +20,12 @@ import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 import static io.hexlet.typoreporter.web.Routers.Typo.TYPOS;
 import static io.hexlet.typoreporter.web.Routers.Workspace.API_WORKSPACES;
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @Setter(onMethod_ = @Autowired)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private SecurityProblemSupport problemSupport;
 
@@ -39,32 +42,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new SecurityExceptionHandler();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(workspaceTokenAuthenticationProvider);
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
         http.httpBasic();
 
         http.exceptionHandling()
             .authenticationEntryPoint(problemSupport)
             .accessDeniedHandler(problemSupport);
 
+        http.authenticationProvider(workspaceTokenAuthenticationProvider);
+
         http.authorizeRequests()
+            .antMatchers(GET, "/", "/webjars/**", "/static/**").permitAll()
             .mvcMatchers(POST, API_WORKSPACES + "/*" + TYPOS).authenticated()
-            .anyRequest().permitAll()
+            .anyRequest().permitAll() // TODO remove when login added
             .and()
             .csrf()
             .ignoringRequestMatchers(new AntPathRequestMatcher(API_WORKSPACES + "/*" + TYPOS, POST.name()));
 
-        http.addFilterBefore(new WorkspaceAuthTokenFilter(authenticationManagerBean()), BasicAuthenticationFilter.class);
+        http.addFilterBefore(workspaceAuthTokenFilter(authManager), BasicAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    private WorkspaceAuthTokenFilter workspaceAuthTokenFilter(AuthenticationManager authManager) {
+        return new WorkspaceAuthTokenFilter(authManager);
     }
 }
