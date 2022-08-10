9736259cc59e562d225e34bb574703ae1bb56bb4
alexeiak <alexeiak.dev@gmail.com>
2022-08-10 18:44:04 +0300

    test: [#100] add tests for WorkspaceService. All TODOs in package /service have done.

--- src/test/java/io/hexlet/typoreporter/service/WorkspaceServiceIT.java
+++ src/test/java/io/hexlet/typoreporter/service/WorkspaceServiceIT.java
@@ -3,8 +3,16 @@ package io.hexlet.typoreporter.service;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
+import io.hexlet.typoreporter.domain.workspace.Workspace;
 import io.hexlet.typoreporter.repository.WorkspaceRepository;
+import io.hexlet.typoreporter.service.dto.workspace.CreateWorkspace;
+import io.hexlet.typoreporter.service.dto.workspace.WorkspaceInfo;
 import io.hexlet.typoreporter.test.DBUnitEnumPostgres;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.*;
@@ -12,8 +20,15 @@ import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.*;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
 import static com.github.database.rider.core.api.configuration.Orthography.LOWERCASE;
 import static io.hexlet.typoreporter.test.Constraints.POSTGRES_IMAGE;
+import static io.hexlet.typoreporter.test.factory.EntitiesFactory.WORKSPACE_101_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Testcontainers
@@ -40,5 +55,72 @@ public class WorkspaceServiceIT {
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
         registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
     }
-    //TODO add tests
+
+    @ParameterizedTest
+    @MethodSource("io.hexlet.typoreporter.test.factory.EntitiesFactory#getWorkspaces")
+    void getAllWorkspacesInfoIsSuccessful() {
+        WorkspaceInfo workspaceInfo = service.getAllWorkspacesInfo().stream()
+            .filter(wksInfo -> wksInfo.name().equals(WORKSPACE_101_NAME))
+            .findFirst().orElse(null);
+
+        assertThat(Objects.requireNonNull(workspaceInfo).createdDateAgo()).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @MethodSource("io.hexlet.typoreporter.test.factory.EntitiesFactory#getWorkspaces")
+    void getWorkspaceInfoByNameIsSuccessful() {
+        WorkspaceInfo workspaceInfo = service.getWorkspaceInfoByName(WORKSPACE_101_NAME).orElse(null);
+        assertThat(workspaceInfo != null).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "wks-test")
+    void existsWorkspaceByNameIsSuccessful(final String wksName) {
+        assertThat(service.existsWorkspaceByName(wksName)).isTrue();
+    }
+
+    @Test
+    void createWorkspaceIsSuccessful() {
+        final var newWks = new CreateWorkspace("wks-name-1", "wks desc");
+        WorkspaceInfo workspaceInfo = service.createWorkspace(newWks);
+        assertThat(workspaceInfo.name()).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "wks-test")
+    void updateWorkspaceIsSuccessful() {
+        final var newWks = new CreateWorkspace("wks-name-1", "wks desc");
+        WorkspaceInfo workspaceInfo = service.updateWorkspace(newWks, "wks-test").orElse(null);
+        assertThat(Objects.requireNonNull(workspaceInfo).name()).isEqualTo(newWks.name());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "wks-test")
+    void deleteWorkspaceByNameIsSuccessful(final String wksName) {
+        final Integer successfulCode = 1;
+        assertThat(service.deleteWorkspaceByName(wksName)).isEqualTo(successfulCode);
+        assertThat(service.existsWorkspaceByName(wksName)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "wks-test")
+    void getWorkspaceApiAccessTokenByNameIsSuccessful(final String wksName) {
+        UUID uuid = service.getWorkspaceApiAccessTokenByName(wksName).orElse(null);
+        assertThat(uuid != null).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "wks-test")
+    void regenerateWorkspaceApiAccessTokenByNameIsSuccessful(final String wksName) {
+        UUID previousUuid = service.getWorkspaceApiAccessTokenByName(wksName).orElse(null);
+        UUID newUuid = service.regenerateWorkspaceApiAccessTokenByName(wksName).orElse(null);
+        assertThat(previousUuid).isNotEqualTo(newUuid);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "wks-test")
+    void getWorkspaceByNameIsSuccessful(final String wksName) {
+        Workspace workspace = service.getWorkspaceByName(wksName).orElse(null);
+        assertThat(workspace != null).isTrue();
+    }
 }
