c09fdeebfcb6ac7dd58dc8fb2c6b52225042f349
Nikolay_Yakovlev <n.yakovlev96@gmail.com>
2022-09-22 14:16:55 +0500

    update: make navbar a React component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
