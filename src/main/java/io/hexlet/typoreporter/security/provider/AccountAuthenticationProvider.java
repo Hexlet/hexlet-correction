cc00e14ff5684f1efc189bf2d5aad88d1e1f7f07
Kvas <shokhin.al@gmail.com>
2022-09-28 16:14:52 +0300

    PR feedback: AccountAuthProvider

--- src/main/java/io/hexlet/typoreporter/security/provider/AccountAuthenticationProvider.java
+++ src/main/java/io/hexlet/typoreporter/security/provider/AccountAuthenticationProvider.java
@@ -3,25 +3,43 @@ package io.hexlet.typoreporter.security.provider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AccountAuthenticationProvider implements AuthenticationProvider {
+public class AccountAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
 
     private final PasswordEncoder encoder;
 
     private final UserDetailsService service;
+    private volatile String userNotFoundEncodedPassword;
 
     public AccountAuthenticationProvider(@Qualifier("accountDetailService") UserDetailsService service,
                                          @Qualifier("bCryptPasswordEncoder") PasswordEncoder encoder) {
         this.encoder = encoder;
         this.service = service;
+    }
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails,
+                                                  UsernamePasswordAuthenticationToken auth) throws AuthenticationException {
+      if (auth.getCredentials() == null) {
+          throw new BadCredentialsException("Failed to authenticate. No credentials provided");
+      }
+
+      final String password = auth.getCredentials().toString();
+      if (!encoder.matches(password, userDetails.getPassword())) {
+          throw new BadCredentialsException("Failed to authenticate. Password is wrong.");
+      }
     }
 
     @Override
@@ -39,7 +57,43 @@ public class AccountAuthenticationProvider implements AuthenticationProvider {
     }
 
     @Override
+    protected UserDetails retrieveUser(String username,
+                                       UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        prepareTimingAttackProtection();
+
+        try {
+            final UserDetails securedAccount = service.loadUserByUsername(username);
+            if (securedAccount == null) {
+                throw new InternalAuthenticationServiceException("UserDetailsService returned null, which is an interface contract violation");
+            }
+            return securedAccount;
+        } catch (UsernameNotFoundException var4) {
+            mitigateAgainstTimingAttack(authentication);
+            throw var4;
+        } catch (InternalAuthenticationServiceException var5) {
+            throw var5;
+        } catch (Exception var6) {
+            throw new InternalAuthenticationServiceException(var6.getMessage(), var6);
+        }
+    }
+
+    @Override
     public boolean supports(Class<?> auth) {
         return auth.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+    private void prepareTimingAttackProtection() {
+        if (userNotFoundEncodedPassword == null) {
+            userNotFoundEncodedPassword = encoder.encode("userNotFoundPassword");
+        }
+
+    }
+
+    private void mitigateAgainstTimingAttack(UsernamePasswordAuthenticationToken authentication) {
+        if (authentication.getCredentials() != null) {
+            String presentedPassword = authentication.getCredentials().toString();
+            encoder.matches(presentedPassword, this.userNotFoundEncodedPassword);
+        }
+
     }
 }
