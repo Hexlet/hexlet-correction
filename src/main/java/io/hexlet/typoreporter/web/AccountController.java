42123dbbfc8267cf7c6c1a7cd7a31df924adb114
nikita <nikisysoev@gmail.com>
2022-11-06 19:28:03 +0700

    PR feedback: add AccountAlreadyExistException

--- src/main/java/io/hexlet/typoreporter/web/AccountController.java
+++ src/main/java/io/hexlet/typoreporter/web/AccountController.java
@@ -2,24 +2,19 @@ package io.hexlet.typoreporter.web;
 
 import io.hexlet.typoreporter.domain.account.Account;
 import io.hexlet.typoreporter.service.AccountService;
-import io.hexlet.typoreporter.service.QueryAccount;
 import io.hexlet.typoreporter.service.dto.account.UpdatePassword;
 import io.hexlet.typoreporter.service.dto.account.UpdateProfile;
-import io.hexlet.typoreporter.web.exception.EmailAlreadyExistException;
+import io.hexlet.typoreporter.web.exception.AccountAlreadyExistException;
 import io.hexlet.typoreporter.web.exception.OldPasswordWrongException;
-import io.hexlet.typoreporter.web.exception.UsernameAlreadyExistException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.zalando.problem.AbstractThrowableProblem;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
@@ -34,10 +29,6 @@ import static io.hexlet.typoreporter.web.Templates.*;
 public class AccountController {
 
     private final AccountService accountService;
-
-    private final QueryAccount queryAccount;
-
-    private final PasswordEncoder passwordEncoder;
 
     private final Authentication authentication;
 
@@ -79,23 +70,6 @@ public class AccountController {
 
         model.addAttribute("formModified", true);
 
-        final String name = authentication.getName();
-        final var sourceAccount = accountService.getAccount(name);
-        if (sourceAccount.isEmpty()) {
-            //TODO send to error page
-            log.error("Account not found");
-            return REDIRECT_ROOT;
-        }
-
-        final String username = sourceAccount.get().getUsername();
-        if (!username.equals(updateProfile.getUsername()) && queryAccount.existsByUsername(updateProfile.getUsername())) {
-            bindingResult.addError(UsernameAlreadyExistException.fieldNameError(updateProfile));
-        }
-        final String email = sourceAccount.get().getEmail();
-        if (!email.equals(updateProfile.getEmail()) && queryAccount.existsByEmail(updateProfile.getEmail())) {
-            bindingResult.addError(EmailAlreadyExistException.fieldNameError(updateProfile));
-        }
-
         if (bindingResult.hasErrors()) {
             model.addAttribute("updateProfile", updateProfile);
             return PROF_UPDATE_TEMPLATE;
@@ -103,15 +77,15 @@ public class AccountController {
 
         Optional<Account> updatedAccount;
         try {
+            final String name = authentication.getName();
             updatedAccount = accountService.updateProfile(updateProfile, name);
             if (updatedAccount.isEmpty()) {
                 //TODO send to error page
                 log.error("Account not found");
                 return REDIRECT_ROOT;
             }
-            // never happens
-        } catch (AbstractThrowableProblem e) {
-            log.error("Failed to update account profile");
+        } catch (AccountAlreadyExistException e) {
+            bindingResult.addError(e.toFieldError("updateProfile"));
             return PROF_UPDATE_TEMPLATE;
         }
 
@@ -130,20 +104,12 @@ public class AccountController {
         return PASS_UPDATE_TEMPLATE;
     }
 
-
     @PutMapping(PASSWORD + UPDATE)
     public String putPasswordUpdate(final Model model,
                                    final @Valid @ModelAttribute("updatePassword") UpdatePassword updatePassword,
                                    final BindingResult bindingResult) {
 
-
         model.addAttribute("formModified", true);
-
-        final String name = authentication.getName();
-        final String password = authentication.getCredentials().toString();
-        if (!passwordEncoder.matches(updatePassword.getOldPassword(), password)) {
-            bindingResult.addError(OldPasswordWrongException.fieldNameError());
-        }
 
         if (bindingResult.hasErrors()) {
             model.addAttribute("updatePassword", updatePassword);
@@ -152,15 +118,15 @@ public class AccountController {
 
         Optional<Account> updatedAccount;
         try {
+            final String name = authentication.getName();
             updatedAccount = accountService.updatePassword(updatePassword, name);
             if (updatedAccount.isEmpty()) {
                 //TODO send to error page
                 log.error("Account not found");
                 return REDIRECT_ROOT;
             }
-            // never happens
-        } catch (AbstractThrowableProblem e) {
-            log.error("Failed to update account password");
+        } catch (OldPasswordWrongException e) {
+            bindingResult.addError(e.toFieldError());
             return PASS_UPDATE_TEMPLATE;
         }
 
