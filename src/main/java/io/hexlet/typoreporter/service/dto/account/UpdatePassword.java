c8c3acf7ae24822543afc9820e3e722b96a7d7aa
nikita <nikisysoev@gmail.com>
2022-11-06 17:28:16 +0700

    PR feedback: add cross-field validation

--- src/main/java/io/hexlet/typoreporter/service/dto/account/UpdatePassword.java
+++ src/main/java/io/hexlet/typoreporter/service/dto/account/UpdatePassword.java
@@ -1,6 +1,7 @@
 package io.hexlet.typoreporter.service.dto.account;
 
 import io.hexlet.typoreporter.domain.account.constraint.AccountPassword;
+import io.hexlet.typoreporter.domain.account.constraint.PasswordsMustMatch;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -8,6 +9,7 @@ import lombok.experimental.Accessors;
 @Getter
 @Setter
 @Accessors(chain = true)
+@PasswordsMustMatch(message = "The password fields must match")
 public class UpdatePassword {
 
     private String oldPassword;
