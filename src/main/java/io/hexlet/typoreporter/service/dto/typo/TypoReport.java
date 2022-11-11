4415025a673985a3583ef66d0ee6cb4f9857ee25
Artem Gorbounov <artem.gorbounov@gmail.com>
2022-11-12 00:22:20 +0100

    Add widget controller

--- src/main/java/io/hexlet/typoreporter/service/dto/typo/TypoReport.java
+++ src/main/java/io/hexlet/typoreporter/service/dto/typo/TypoReport.java
@@ -1,6 +1,11 @@
 package io.hexlet.typoreporter.service.dto.typo;
 
-import io.hexlet.typoreporter.domain.typo.constraint.*;
+import io.hexlet.typoreporter.domain.typo.constraint.ReporterComment;
+import io.hexlet.typoreporter.domain.typo.constraint.ReporterName;
+import io.hexlet.typoreporter.domain.typo.constraint.TextAfterTypo;
+import io.hexlet.typoreporter.domain.typo.constraint.TextBeforeTypo;
+import io.hexlet.typoreporter.domain.typo.constraint.TextTypo;
+import io.hexlet.typoreporter.domain.typo.constraint.TypoPageUrl;
 
 public record TypoReport(
     @TypoPageUrl
@@ -17,4 +22,7 @@ public record TypoReport(
     String textAfterTypo
 ) {
 
+    public static TypoReport empty() {
+        return new TypoReport("", "", "", "", "", "");
+    }
 }
---
+++ src/main/java/io/hexlet/typoreporter/web/WidgetController.java
@@ -0,0 +1,83 @@
+package io.hexlet.typoreporter.web;
+
+import io.hexlet.typoreporter.service.TypoService;
+import io.hexlet.typoreporter.service.WorkspaceService;
+import io.hexlet.typoreporter.service.dto.typo.TypoReport;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class WidgetController {
+
+    private final WorkspaceService workspaceService;
+
+    private final TypoService typoService;
+
+    @GetMapping("/typo/form/{wksName}")
+    String getWidgetTypoForm(HttpServletResponse response,
+                             final Model model,
+                             @PathVariable String wksName) {
+        final var wks = workspaceService.getWorkspaceByName(wksName);
+        if (wks.isEmpty()) {
+            log.error("Error during sending widget typo form. Workspace not found");
+            return "widget/report-typo-error";
+        }
+        final var workspace = wks.get();
+
+        response.addHeader("Content-Security-Policy", "frame-ancestors " + workspace.getUrl());
+        model.addAttribute("trustedOrigin", workspace.getUrl());
+
+        model.addAttribute("wksName", workspace.getName());
+        model.addAttribute("formModified", false);
+        model.addAttribute("typoReport", TypoReport.empty());
+
+        log.info("Send widget typo form to '{}'", workspace.getUrl());
+        return "widget/typo-form";
+    }
+
+    @PostMapping("/typo/form/{wksName}")
+    String postWidgetTypoForm(final Model model,
+                              @Valid @ModelAttribute TypoReport typoReport,
+                              BindingResult bindingResult,
+                              @PathVariable String wksName) {
+        final var wksExists = workspaceService.existsWorkspaceByName(wksName);
+
+        if (!wksExists) {
+            log.error("Error during saving typo from widget. Workspace not found");
+            return "widget/report-typo-error";
+        }
+
+        if (bindingResult.hasFieldErrors("reporterComment")) {
+            log.warn("Validation error during saving typo from widget. Typo not valid. Errors: {}", bindingResult.getAllErrors());
+            model.addAttribute("typoReport", typoReport);
+            model.addAttribute("formModified", true);
+            return "widget/typo-form";
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.error("Validation error during saving typo from widget. Typo not valid. Errors: {}", bindingResult.getAllErrors());
+            return "widget/report-typo-error";
+        }
+
+        try {
+            typoService.addTypoReport(typoReport, wksName);
+        } catch (Exception e) {
+            log.error("Error during saving typo from widget.", e);
+            return "widget/report-typo-error";
+        }
+
+        return "widget/report-typo-success";
+    }
+}
