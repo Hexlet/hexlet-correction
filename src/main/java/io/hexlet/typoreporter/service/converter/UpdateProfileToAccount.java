8ace3e0cfb99620d49134486d400d33b0c4bdabf
nikita <nikisysoev@gmail.com>
2022-11-24 19:50:50 +0700

    add UpdateProfileToAccount converter

---
+++ src/main/java/io/hexlet/typoreporter/service/converter/UpdateProfileToAccount.java
@@ -0,0 +1,21 @@
+package io.hexlet.typoreporter.service.converter;
+
+import io.hexlet.typoreporter.domain.account.Account;
+import io.hexlet.typoreporter.service.dto.account.UpdateProfile;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.util.Pair;
+
+public class UpdateProfileToAccount implements Converter<Pair<Account, UpdateProfile>, Account> {
+
+    // TODO docs
+    @Override
+    public Account convert(final Pair<Account, UpdateProfile> pair) {
+        final UpdateProfile updateProfile = pair.getSecond();
+
+        return pair.getFirst()
+            .setUsername(updateProfile.getUsername())
+            .setEmail(updateProfile.getEmail())
+            .setFirstName(updateProfile.getFirstName())
+            .setLastName(updateProfile.getLastName());
+    }
+}
