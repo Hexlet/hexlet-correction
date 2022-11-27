6c5d16b311dd4347d8dbf0e27f403cc601d0e879
Mila Zolotova <l.zolotova@yahoo.com>
2022-11-28 01:24:24 +0300

    refactor

--- src/main/resources/templates/widget/index.js
+++ src/main/resources/templates/widget/index.js
@@ -5,25 +5,41 @@ document.addEventListener('DOMContentLoaded', function () {
   const popupContent = document.querySelector('.popup__content');
   const btnClose = document.querySelector('.btn-secondary');
   const form = document.querySelector('.total-form');
+  const pageUrl = document.getElementById('pageUrl');
+  // const reporterName = document.getElementById('reporterName');
   const textBeforeTypo = document.getElementById('textBeforeTypo');
   const textTypo = document.getElementById('textTypo');
   const textAfterTypo = document.getElementById('textAfterTypo');
 
-  function getSelectedText() {
-    if (window.getSelection()) {
-      const select = window.getSelection();
-      return select.toString();
-    }
+  const state = {
+    text: '',
   }
-
   document.addEventListener('keydown', function (e) {
-    const text = getSelectedText();
-    if ((e.keyCode === 13 && e.ctrlKey) && text) {
-      e.preventDefault();
-      popup.classList.add('shown');
-    }
+    if (e.keyCode === 13 && e.ctrlKey) {
+      state.text = window.getSelection().toString();
+      if (state.text.length) {
+        e.preventDefault();
+        popup.classList.add('shown');
+        const range = new Range();
+        pageUrl.value = range.startContainer.baseURI;
+        textBeforeTypo.value = state.text;
+        textBeforeTypo.disabled = 'disabled';
+        textTypo.value = state.text;
+        textTypo.disabled = 'disabled';
+        textAfterTypo.value = state.text;
+        textAfterTypo.classList.add('outline-red');
+        textAfterTypo.addEventListener('input', (e) => {
+          e.preventDefault();
+          if (textTypo.value !== textAfterTypo.value) {
+            textAfterTypo.classList.remove('outline-red');
+          } else {
+            textAfterTypo.classList.add('outline-red');
+          }
 
-  });
+        })
+      }
+    }
+  })
   const closePopup = () => {
     popup.classList.remove('shown');
     form.reset();
