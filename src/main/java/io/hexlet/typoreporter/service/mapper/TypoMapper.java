2de96780ad29cac6c2385f2e8a2ae59a41394c32
Artem Gorbounov <artem.gorbounov@gmail.com>
2023-02-14 15:23:13 +0100

    use general mapper for pretty date mapping, add date mapping into TypoMapper

--- src/main/java/io/hexlet/typoreporter/service/mapper/TypoMapper.java
+++ src/main/java/io/hexlet/typoreporter/service/mapper/TypoMapper.java
@@ -6,11 +6,15 @@ import io.hexlet.typoreporter.service.dto.typo.TypoInfo;
 import io.hexlet.typoreporter.service.dto.typo.TypoReport;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(uses = PrettyDateMapper.class)
 public interface TypoMapper {
     TypoReport toTypoReport(Typo source);
     Typo toTypo(TypoReport source);
+    @Mapping(target = "modifiedDateAgo", source = "modifiedDate", qualifiedByName = "mapToPrettyDateAgo")
+    @Mapping(target = "createdDateAgo", source = "createdDate", qualifiedByName = "mapToPrettyDateAgo")
+    @Mapping(target = "typoStatusStr", source = "typoStatus")
     TypoInfo toTypoInfo(Typo source);
     Typo toTypo(TypoInfo source);
     ReportedTypo toReportedTypo(Typo source);
--- src/main/java/io/hexlet/typoreporter/service/mapper/WorkspaceMapper.java
+++ src/main/java/io/hexlet/typoreporter/service/mapper/WorkspaceMapper.java
@@ -3,29 +3,17 @@ package io.hexlet.typoreporter.service.mapper;
 import io.hexlet.typoreporter.domain.workspace.Workspace;
 import io.hexlet.typoreporter.service.dto.workspace.CreateWorkspace;
 import io.hexlet.typoreporter.service.dto.workspace.WorkspaceInfo;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.ocpsoft.prettytime.PrettyTime;
 
-import java.time.LocalDateTime;
-
-@Mapper
+@Mapper(uses = PrettyDateMapper.class)
 public interface WorkspaceMapper {
     CreateWorkspace toCreateWorkspace(Workspace source);
 
     Workspace toWorkspace(CreateWorkspace source);
     Workspace toWorkspace(WorkspaceInfo source);
 
-    @Mapping(target = "createdDateAgo", source = "createdDate")
-    @Mapping(target = "modifiedDateAgo", source = "modifiedDate")
+    @Mapping(target = "createdDateAgo", source = "createdDate", qualifiedByName = "mapToPrettyDateAgo")
+    @Mapping(target = "modifiedDateAgo", source = "modifiedDate", qualifiedByName = "mapToPrettyDateAgo")
     WorkspaceInfo toWorkspaceInfo(Workspace source);
-
-    default String getDateAgoAsString(LocalDateTime date) {
-        final PrettyTime pt = new PrettyTime();
-        return pt.format(date);
-    }
-
 }
-
-
