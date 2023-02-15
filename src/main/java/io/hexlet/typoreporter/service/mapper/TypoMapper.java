ac72955376daf220ec9320f978d787a304281fdd
Artem Gorbounov <artem.gorbounov@gmail.com>
2023-02-15 09:08:34 +0100

    fix tests

--- src/main/java/io/hexlet/typoreporter/service/mapper/TypoMapper.java
+++ src/main/java/io/hexlet/typoreporter/service/mapper/TypoMapper.java
@@ -7,9 +7,15 @@ import io.hexlet.typoreporter.service.dto.typo.TypoReport;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.ocpsoft.prettytime.PrettyTime;
 
-@Mapper(uses = PrettyDateMapper.class)
+import java.time.LocalDateTime;
+
+@Mapper
 public interface TypoMapper {
+
+    PrettyTime prettyTime = new PrettyTime();
 
     Typo toTypo(TypoReport source);
 
@@ -19,4 +25,9 @@ public interface TypoMapper {
     TypoInfo toTypoInfo(Typo source);
 
     ReportedTypo toReportedTypo(Typo source);
+
+    @Named(value = "mapToPrettyDateAgo")
+    default String getDateAgoAsString(LocalDateTime date) {
+        return prettyTime.format(date);
+    }
 }
--- src/main/java/io/hexlet/typoreporter/service/mapper/WorkspaceMapper.java
+++ src/main/java/io/hexlet/typoreporter/service/mapper/WorkspaceMapper.java
@@ -5,13 +5,24 @@ import io.hexlet.typoreporter.service.dto.workspace.CreateWorkspace;
 import io.hexlet.typoreporter.service.dto.workspace.WorkspaceInfo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.ocpsoft.prettytime.PrettyTime;
 
-@Mapper(uses = PrettyDateMapper.class)
+import java.time.LocalDateTime;
+
+@Mapper
 public interface WorkspaceMapper {
+
+    PrettyTime prettyTime = new PrettyTime();
 
     Workspace toWorkspace(CreateWorkspace source);
 
     @Mapping(target = "createdDateAgo", source = "createdDate", qualifiedByName = "mapToPrettyDateAgo")
     @Mapping(target = "modifiedDateAgo", source = "modifiedDate", qualifiedByName = "mapToPrettyDateAgo")
     WorkspaceInfo toWorkspaceInfo(Workspace source);
+
+    @Named(value = "mapToPrettyDateAgo")
+    default String getDateAgoAsString(LocalDateTime date) {
+        return prettyTime.format(date);
+    }
 }
