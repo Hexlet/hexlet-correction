18011fe84cd68b482a74cafe1b31329e8bbce853
Nikita Ionov <ionov.goo@gmail.com>
2023-02-24 00:14:19 +0300

    renamed field accounts to workspaceRoles; changed add methods

--- src/test/java/io/hexlet/typoreporter/service/mapper/CreateWorkspaceToWorkspaceTest.java
+++ src/test/java/io/hexlet/typoreporter/service/mapper/CreateWorkspaceToWorkspaceTest.java
@@ -17,7 +17,7 @@ class CreateWorkspaceToWorkspaceTest {
     void requestReportToTypo(final CreateWorkspace createWorkspace) {
         final var workspace = workspaceMapper.toWorkspace(createWorkspace);
         assertThat(workspace).usingRecursiveComparison()
-            .ignoringFields("id", "apiAccessToken", "typos", "createdDate", "createdBy", "modifiedDate", "modifiedBy", "accounts", "accounts")
+            .ignoringFields("id", "apiAccessToken", "typos", "createdDate", "createdBy", "modifiedDate", "modifiedBy", "workspaceRoles", "accounts")
             .isEqualTo(createWorkspace);
     }
 }
