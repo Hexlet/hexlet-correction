c698d2b6f406aaedc54859245dea4721a48e3bd0
redgradient <neutral1shade@yahoo.com>
2023-05-13 22:06:39 +0400

    add CustomAccessDeniedHandler.java

---
+++ src/main/java/io/hexlet/typoreporter/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package io.hexlet.typoreporter.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        response.sendRedirect("/workspaces");
+    }
+}
