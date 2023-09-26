ed23f34f62c8006c888cc57ab1486d8510e4fa16
Pavel Vasilyev <pavel.vas@mail.ru>
2023-09-26 15:20:01 +0300

    Add limitation for password

--- src/main/java/io/hexlet/typoreporter/domain/account/constraint/AccountPassword.java
+++ src/main/java/io/hexlet/typoreporter/domain/account/constraint/AccountPassword.java
@@ -3,7 +3,7 @@ package io.hexlet.typoreporter.domain.account.constraint;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 
 
 @NotBlank
-@Size(min = 8)
+@Pattern(regexp = "\\p{Graph}{8,20}", message = "{alert.password-wrong-format}")
 @Constraint(validatedBy = {})
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
