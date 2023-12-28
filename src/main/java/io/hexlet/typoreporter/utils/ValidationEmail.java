f08a803780a59899a524b1707806e92144e4353b
suhoi <sushilyaz2000@gmail.com>
2023-12-28 21:50:32 +0300

    add validation email

---
+++ src/main/java/io/hexlet/typoreporter/utils/ValidationEmail.java
@@ -0,0 +1,12 @@
+package io.hexlet.typoreporter.utils;
+
+import java.util.regex.*;
+
+public class ValidationEmail {
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
