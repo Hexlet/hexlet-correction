0185d3a77a8ccb67a3f2eba4720558ffd47b25ed
uchitsa <uchitsa@ya.ru>
2024-08-19 01:59:59 +0300

    Update UpdateProfile.java

--- src/main/java/io/hexlet/typoreporter/service/dto/account/UpdateProfile.java
+++ src/main/java/io/hexlet/typoreporter/service/dto/account/UpdateProfile.java
@@ -2,7 +2,6 @@ package io.hexlet.typoreporter.service.dto.account;
 
 import io.hexlet.typoreporter.domain.account.constraint.AccountUsername;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
