2e11a99648a1f23b90343e3aab9030a12fad2dda
Veronika Patakyová <veronika.patakyova@student.tuke.sk>
2024-10-03 17:02:31 +0200

    Created new user details class to keep track of nickname alongside of email after login and added the username to the navbar

---
+++ src/main/java/io/hexlet/typoreporter/service/dto/account/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package io.hexlet.typoreporter.service.dto.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private String email;
+    private String password;
+    @Getter
+    private final String nickname;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
