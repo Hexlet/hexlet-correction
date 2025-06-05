62f260e91a84f661663f0164e42dc2145c0b6e37
Sanapol <junrey@list.ru>
2025-06-06 01:25:23 +0300

    fix SecurityConfig.java

--- src/main/java/io/hexlet/typoreporter/config/SecurityConfig.java
+++ src/main/java/io/hexlet/typoreporter/config/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
