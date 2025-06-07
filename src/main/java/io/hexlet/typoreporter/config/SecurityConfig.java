2c2ddd4ff92e0101fddd516f16b51343fb9bf4a8
Sanapol <junrey@list.ru>
2025-06-07 17:12:58 +0300

    fix signup error

--- src/main/java/io/hexlet/typoreporter/config/SecurityConfig.java
+++ src/main/java/io/hexlet/typoreporter/config/SecurityConfig.java
@@ -25,7 +25,6 @@ import org.springframework.security.web.context.DelegatingSecurityContextReposit
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -85,14 +84,6 @@ public class SecurityConfig {
         http.httpBasic();
         http.cors();
         http.exceptionHandling().accessDeniedHandler(accessDeniedHandler());
-
-        http.csrf(csrf -> csrf
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-        ).csrf(csrf -> csrf
-            .ignoringRequestMatchers(
-                "/login/oauth2/code/**",
-                "/oauth2/authorization/**")
-        );
 
         http.authorizeHttpRequests(authz -> authz
                 .requestMatchers(GET, "/webjars/**", "/widget/**", "/fragments/**", "/img/**",
