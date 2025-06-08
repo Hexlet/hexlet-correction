ac80d85e5876edd45527a01b653ec557e24a7a12
Sanapol <junrey@list.ru>
2025-06-08 16:48:42 +0300

    remove logging

--- src/main/java/io/hexlet/typoreporter/handler/exception/DuplicateYandexIdException.java
+++
@@ -1,7 +0,0 @@
-package io.hexlet.typoreporter.handler.exception;
-
-public class DuplicateYandexIdException extends RuntimeException {
-    public DuplicateYandexIdException(String message) {
-        super(message);
-    }
-}
