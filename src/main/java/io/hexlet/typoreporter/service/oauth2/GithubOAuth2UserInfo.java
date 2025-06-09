dc0418279895aedb8ebda5da1b6e37cca5159ce7
Sanapol <junrey@list.ru>
2025-06-09 14:39:16 +0300

    update oauth2

--- src/main/java/io/hexlet/typoreporter/service/oauth2/GithubOAuth2UserInfo.java
+++ src/main/java/io/hexlet/typoreporter/service/oauth2/GithubOAuth2UserInfo.java
@@ -4,7 +4,6 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
